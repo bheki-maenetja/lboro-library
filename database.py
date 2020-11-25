7df4b2fc9b0b24a4daf1537d5ae0926e787f0bd2
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-25 16:41:58 +0000

    working on database manager file

--- database.py
+++ database.py
@@ -5,31 +5,17 @@ import re
 from random import choice
 
 # Loading files
-# json_file = open("data.json", "r")
-# text_file = open("database.txt", "a")
-# data = json.load(json_file)
 
-# print(type(data), type(data[0]['category']))
-# input("Wait >> ")
-# for book in data:
-#     book['member_id'] = None
-#     book['isbn'] = book['isbn'].replace('-', '').replace('X', '0')
-#     text_file.write(json.dumps(book) + "\n")
+# Get Index
+def get_all_books():
+    try:
+        database = open("database.txt", "r")
+        return [json.loads(book) for book in database]
+    except:
+        print('Something went wrong...')
+    database.close()
 
-# text_file.close()
-# json_file.close()
-# input("Load data into database >>> ")
-# database = open("database.txt", "r+")
-# database.seek(0)
+print(get_all_books())
 
-# book_index = [json.loads(book) for book in database]
-# for book in book_index:
-#     for other_book in book_index:
-#         if book['title'] == other_book['title'] and (book['isbn'] != other_book['isbn']) and (book['author'] != other_book['author']):
-#             other_book['isbn'] = book['isbn']
-#             other_book['author'] = book ['author']
-#             print('changed a book')
-#     database.write(json.dumps(book) + "\n")
 
-# database.close()
 
