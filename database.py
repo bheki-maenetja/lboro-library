8df6264866ee2ce00e78c4977df398230edb41f1
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-25 18:24:30 +0000

    implemented search functionality programmatically

--- database.py
+++ database.py
@@ -16,17 +16,25 @@ def get_all_books():
 
 def search_books(search_phrase, categories=None):
     book_index = category_match(get_all_books(), categories)
-    return [result_match(book, search_phrase, categories) for book in book_index]
+    search_results = [result_match(book, search_phrase, categories) for book in book_index]
+    return sorted([result for result in search_results if result != None], key=lambda x: x[0])
 
 def category_match(book_index, selected_categories=None):
     if selected_categories == None:
         return book_index
-    return [book for book in book_index if any(cat for cat in book['category'] if cat in selected_categories)]
+    return [book for book in book_index if set(selected_categories).issubset(set(book['category']))]
 
 def result_match(book_obj, search_phrase, categories):
-    if book_obj['title'] == search_phrase:
+    book_title = book_obj['title'].lower()
+    search_phrase = search_phrase.lower()
+    
+    if search_phrase == '':
         return (1, book_obj)
-    elif (book_obj['title'] in search_phrase or search_phrase in book_obj['title']) and search_phrase != '':
+    elif book_title == search_phrase:
+        return (1, book_obj)
+    elif book_title in search_phrase or search_phrase in book_title:
+        return (2, book_obj)
+    elif len(search_phrase) >= 3 and search_phrase[:3] == book_title[:3]:
         return (3, book_obj)
 
 # Single View
@@ -53,6 +61,7 @@ def update_book(book_id, book_obj):
     except:
         print('something went wrong...')
 
+print(search_books('programming', categories=['textbook', 'technology']))
 
 
 
