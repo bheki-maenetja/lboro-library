08cc46cd4487f08bcf18771b559a60aa1f4cee7c
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-25 20:53:35 +0000

    completed functionality to handle book data - still need to do logs

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from random import choice
 
 # Index View
 ## GET
-def get_all_books():
+def get_all_books(): # return all books in the database
     try:
         database = open("database.txt", "r")
         return [json.loads(book) for book in database]
@@ -14,17 +14,17 @@ def get_all_books():
         print('Something went wrong...')
     database.close()
 
-def search_books(search_phrase, categories=None):
+def search_books(search_phrase, categories=None): # returns books based on search parameters
     book_index = category_match(get_all_books(), categories)
     search_results = [result_match(book, search_phrase, categories) for book in book_index]
     return sorted([result for result in search_results if result != None], key=lambda x: x[0])
 
-def category_match(book_index, selected_categories=None):
+def category_match(book_index, selected_categories=None): # filters books based on selected categories
     if selected_categories == None:
         return book_index
     return [book for book in book_index if set(selected_categories).issubset(set(book['category']))]
 
-def result_match(book_obj, search_phrase, categories):
+def result_match(book_obj, search_phrase, categories): # filters books based on search string
     book_title = book_obj['title'].lower()
     search_phrase = search_phrase.lower()
     
@@ -39,7 +39,7 @@ def result_match(book_obj, search_phrase, categories):
 
 # Single View
 ## GET
-def get_book_by_id(book_id):
+def get_book_by_id(book_id): # returns a book based on its id
     try:
         database = open("database.txt", "r")
         for book in database:
@@ -50,7 +50,7 @@ def get_book_by_id(book_id):
         print('something went wrong...')
 
 ## PUT
-def update_book(book_id, book_obj):
+def update_book(book_id, book_obj): # updates a book
     try:
         for book in fileinput.input("database.txt", inplace=True):
             book_dict = json.loads(book)
@@ -61,7 +61,9 @@ def update_book(book_id, book_obj):
     except:
         print('something went wrong...')
 
-print(search_books('programming', categories=['textbook', 'technology']))
+my_book = get_book_by_id(20)
+my_book['member_id'] = 5555
+update_book(20, my_book)
 
 
 
