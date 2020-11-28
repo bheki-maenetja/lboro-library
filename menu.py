2114895f384ac813608ccac6b5160702d7e0091c
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-28 20:01:57 +0000

    built basic hero section for home page

--- menu.py
+++ menu.py
@@ -1,8 +1,11 @@
 import tkinter as tk
 import tkinter.font as tkFont
 
+from datetime import datetime as dt
+
 # ============================================================ HOME PAGE ============================================================
 root = tk.Tk()
+root.title('Loughborough Library Management System - Firat Batmaz')
 root.geometry('900x630')
 root.minsize(600, 420)
 root.maxsize(1350, 945)
@@ -14,12 +17,29 @@ def build_home_page():
     home_frame.rowconfigure(0, weight=1, minsize=root.winfo_width())
     home_frame.rowconfigure(1, weight=1, minsize=root.winfo_width())
 
-    hero_section = tk.Frame(master=home_frame, bg="blue")
-    button_section = tk.Frame(master=home_frame, bg="green")
+    hero_section = build_hero_section(home_frame)
+    button_section = tk.Frame(master=home_frame, bg="grey")
     hero_section.grid(row=0, column=0, sticky="nesw")
     button_section.grid(row=1, column=0, sticky="nesw")
 
     return home_frame
+
+def build_hero_section(master_frame):
+    hero_section = tk.Frame(master=master_frame, bg="purple")
+    hero_section.rowconfigure(0, weight=1, minsize=root.winfo_width())
+    hero_section.rowconfigure(1, weight=1, minsize=root.winfo_width())
+    hero_section.columnconfigure(0, weight=1, minsize=root.winfo_height())
+
+    heading_font = tkFont.Font(family="Verdana", size=40)
+    sub_heading_font = tkFont.Font(family="Verdana", size=20)
+
+    heading = tk.Label(master=hero_section, text="Loughborough Library", font=heading_font, bg="purple", fg="magenta")
+    sub_heading = tk.Label(master=hero_section, text=f"{dt.strftime(dt.now(), '%d %B, %Y')}", font=sub_heading_font, bg="purple", fg="magenta")
+
+    heading.grid(row=0, column=0, sticky="ws", padx=10)
+    sub_heading.grid(row=1, column=0, sticky="wn", padx=15)
+
+    return hero_section
 
 my_home = build_home_page()
 my_home.pack(fill=tk.BOTH, expand=1)
