90c7411d0579a74d7dcb26b1fc3d92abb2a31be9
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-28 19:18:54 +0000

    created main window

---
+++ menu.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+# ============================================================ HOME PAGE ============================================================
+root = tk.Tk()
+root.geometry('900x630')
+root.minsize(600, 420)
+root.maxsize(1350, 945)
+root.aspect(10,7,10,7)
+
+
+if __name__ == '__main__':
+    root.mainloop()
