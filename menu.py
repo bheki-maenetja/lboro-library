72bafc55b79098bb495002a6c33451c61bfac76b
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-28 19:35:50 +0000

    building home page

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.font as tkFont
 
 # ============================================================ HOME PAGE ============================================================
 root = tk.Tk()
@@ -6,6 +7,22 @@ root.geometry('900x630')
 root.minsize(600, 420)
 root.maxsize(1350, 945)
 root.aspect(10,7,10,7)
+
+def build_home_page():
+    home_frame = tk.Frame(master=root, height=100, width=100)
+    home_frame.columnconfigure(0, weight=1, minsize=root.winfo_height())
+    home_frame.rowconfigure(0, weight=1, minsize=root.winfo_width())
+    home_frame.rowconfigure(1, weight=1, minsize=root.winfo_width())
+
+    hero_section = tk.Frame(master=home_frame, bg="blue")
+    button_section = tk.Frame(master=home_frame, bg="green")
+    hero_section.grid(row=0, column=0, sticky="nesw")
+    button_section.grid(row=1, column=0, sticky="nesw")
+
+    return home_frame
+
+my_home = build_home_page()
+my_home.pack(fill=tk.BOTH, expand=1)
 
 
 if __name__ == '__main__':
