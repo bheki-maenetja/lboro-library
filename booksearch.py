d4f3a7d47505f2099e1fa1a65b0c8cdfa616cee1
bheki-maenetja <bhekimaenetja@gmail.com>
2020-12-01 19:17:14 +0000

    moved code into booksearch

--- booksearch.py
+++ booksearch.py
@@ -1,1 +1,42 @@
+# Standard Library Imports
+import tkinter as tk
+
 book_categories = ("non-fiction", "fiction", "textbook", "novel", "short story", "languages", "technology", "art", "social", "business", "programing", "philosophy")
+# UI Components for Searching Books
+def build_books_page(master_frame):
+    books_page = tk.Frame(master=master_frame)
+    books_page.columnconfigure(0, weight=1, minsize=10)
+    books_page.rowconfigure(0, weight=1, minsize=10)
+    books_page.rowconfigure(1, weight=1, minsize=10)
+    books_page.rowconfigure(2, weight=1, minsize=10)
+
+    category_section = build_category_section(books_page)
+    search_section = build_search_section(books_page)
+    search_section.grid(row=0, column=0, sticky="nsew")
+    category_section.grid(row=1, column=0, sticky="new")
+    return books_page
+
+def build_category_section(master_frame):
+    category_section = tk.Frame(master_frame, bg="grey")
+
+    for i in range(4):
+        category_section.columnconfigure(i, weight=1, minsize=70)
+        for j in range(3):
+            category_section.rowconfigure(j, weight=1, minsize=30)
+            index = i + 4*j
+            new_checkbox = tk.Checkbutton(master=category_section, text=book_categories[index], onvalue="on", offvalue="off", bg="green")
+            new_checkbox.deselect()
+            new_checkbox.grid(row=j, column=i, padx=0, pady=0, sticky="w")
+    
+    return category_section
+
+def build_search_section(master_frame):
+    search_section = tk.Frame(master=master_frame, bg="darkgrey")
+    search_bar = tk.Entry(master=search_section)
+    search_button = tk.Button(master=search_section, text="Search")
+    search_bar.pack(fill=tk.X, expand=3, side=tk.LEFT)
+    search_button.pack(fill=tk.X, expand=1, side=tk.LEFT)
+
+    return search_section
+
+# Functionality Components for Searching Books
--- menu.py
+++ menu.py
@@ -78,7 +78,7 @@ def build_page_container():
     page_notebook = ttk.Notebook(master=root)
     page_notebook.add(tk.Frame(), text="Home")
 
-    books_page = build_books_page(page_notebook)
+    books_page = bs.build_books_page(page_notebook)
 
     page_notebook.add(books_page, text="Books")
     # page_notebook.add(tk.Frame(master=page_notebook, bg="blue"), text="Books")
@@ -97,42 +97,7 @@ def go_to_home_page(e):
         transition(to_home=True)
 
 ## Books Page
-def build_books_page(master_frame):
-    books_page = tk.Frame(master=master_frame)
-    books_page.columnconfigure(0, weight=1, minsize=10)
-    books_page.rowconfigure(0, weight=1, minsize=10)
-    books_page.rowconfigure(1, weight=1, minsize=10)
-    books_page.rowconfigure(2, weight=1, minsize=10)
 
-    category_section = build_category_section(books_page)
-    search_section = build_search_section(books_page)
-    search_section.grid(row=0, column=0, sticky="nsew")
-    category_section.grid(row=1, column=0, sticky="new")
-    return books_page
-
-def build_category_section(master_frame):
-    category_section = tk.Frame(master_frame, bg="grey")
-    book_categories = bs.book_categories
-
-    for i in range(4):
-        category_section.columnconfigure(i, weight=1, minsize=70)
-        for j in range(3):
-            category_section.rowconfigure(j, weight=1, minsize=30)
-            index = i + 4*j
-            new_checkbox = tk.Checkbutton(master=category_section, text=book_categories[index], onvalue="on", offvalue="off", bg="green")
-            new_checkbox.deselect()
-            new_checkbox.grid(row=j, column=i, padx=0, pady=0, sticky="w")
-    
-    return category_section
-
-def build_search_section(master_frame):
-    search_section = tk.Frame(master=master_frame, bg="black")
-    search_bar = tk.Entry(master=search_section)
-    search_button = tk.Button(master=search_section, text="Search")
-    search_bar.pack(fill=tk.X, expand=3, side=tk.LEFT)
-    search_button.pack(fill=tk.X, expand=1, side=tk.LEFT)
-
-    return search_section
 
 page_manager['pages_section'] = build_page_container()
 # ============================================================ MOVING BETWEEN PAGES ============================================================
