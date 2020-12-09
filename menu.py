f090bc139a84b6ae405c9be989575a828c906e4c
bheki-maenetja <bhekimaenetja@gmail.com>
2020-12-09 13:22:28 +0000

    rejigged order of functions on menu.py

--- menu.py
+++ menu.py
@@ -41,6 +41,7 @@ def build_page_container():
     page_notebook.bind('<<NotebookTabChanged>>', lambda e: go_to_home_page(e))
 
     return page_notebook
+
 # ========================================================================================= HOME PAGE =========================================================================================
 ## Home Page UI Components ================================================================
 def build_home_page():
@@ -160,6 +161,16 @@ def build_books_page(master_frame):
     results_section.grid(row=4, column=0, sticky="nesw")
     return books_page
 
+def build_search_section(master_frame):
+    global search_bar
+    search_section = tk.Frame(master=master_frame, bg="darkgrey")
+    search_bar = tk.Entry(master=search_section, textvariable=books_page_state['search_var'])
+    search_button = tk.Button(master=search_section, text="Search")
+    search_bar.pack(fill=tk.X, expand=3, side=tk.LEFT)
+    search_button.pack(fill=tk.X, expand=1, side=tk.LEFT)
+
+    return search_section
+
 def build_category_section(master_frame):
     category_section = tk.Frame(master_frame, bg="grey")
 
@@ -181,36 +192,6 @@ def build_category_section(master_frame):
             new_checkbox.grid(row=j, column=i, padx=0, pady=0, sticky="w")
     
     return category_section
-
-def build_search_section(master_frame):
-    global search_bar
-    search_section = tk.Frame(master=master_frame, bg="darkgrey")
-    search_bar = tk.Entry(master=search_section, textvariable=books_page_state['search_var'])
-    search_button = tk.Button(master=search_section, text="Search")
-    search_bar.pack(fill=tk.X, expand=3, side=tk.LEFT)
-    search_button.pack(fill=tk.X, expand=1, side=tk.LEFT)
-
-    return search_section
-
-def build_results_section(master_frame):
-    results_section = tk.Frame(master=master_frame, bg="pink")
-    results_section.rowconfigure(0, weight=1, minsize=10)
-    results_section.columnconfigure(0, weight=1, minsize=10)
-    
-    footer_frame = tk.Frame(master=results_section, bg="purple")
-    previous_button = tk.Button(footer_frame, text="Previous", command=lambda: change_book_results_page(False))
-    next_button = tk.Button(footer_frame, text="Next", command=lambda: change_book_results_page(True))
-    page_label = tk.Label(footer_frame, text="Page")
-
-    books_page_state['page_label'] = page_label
-
-    previous_button.pack(fill=tk.Y, side=tk.LEFT)
-    next_button.pack(fill=tk.Y, side=tk.LEFT)
-    page_label.pack(fill=tk.Y, side=tk.RIGHT)
-
-    footer_frame.pack(fill=tk.X, side=tk.BOTTOM, expand=0)
-    books_page_state['results_section'] = results_section
-    return results_section
 
 def build_checkout_section(master_frame):
     checkout_section = tk.Frame(master=master_frame, bg="navy")
@@ -237,47 +218,6 @@ def build_checkout_section(master_frame):
 
     books_page_state['checkout_form'] = checkout_section
     return checkout_section
-    
-## Books Page Functionality ==============================================================
-def book_search_handler(search_phrase):
-    current_page = books_page_state['current_page']
-
-    if len(current_page) == 3:
-        books_page_state['current_page'][2].destroy()
-
-    search_results = bs.search_handler(search_phrase, books_page_state['selected_categories'])
-    # for key in search_results:
-    #     print(f"Page {key}:", search_results[key], sep="\n")
-
-    if search_results:
-        current_page = [0, search_results[0], None]
-        page_label = f"Page 1 of {len(search_results)}"
-    else:
-        current_page = [0, [], None]
-        page_label = "Page 1 of 1"
-
-    books_page_state['search_results'] = search_results
-    books_page_state['current_page'] = current_page
-    books_page_state['page_label']['text'] = page_label
-
-    build_results_page()
-
-def book_checkout_handler():
-    loan_duration = books_page_state['duration_var'].get()
-    member_id = books_page_state['member_var'].get()
-    selected_books = books_page_state['checkout_books']
-
-    if len(member_id) < 4 or int(member_id) < 1000:
-        alert("Please enter a valid member ID!!!")
-    else:
-        try:
-            member_id, loan_duration = int(member_id), int(loan_duration)
-            bc.checkout_handler(member_id, selected_books, loan_duration)
-            clear_selected_books()
-            book_search_handler(books_page_state['search_var'].get())
-            alert("Success - Book(s) have been checked out", False)
-        except:
-            alert("Error - Books could not be checked out")
 
 def build_header_row(master_frame, headings, is_header=False):
     header_frame = tk.Frame(master=master_frame, bg="red", relief=tk.RAISED)
@@ -287,6 +227,26 @@ def build_header_row(master_frame, headings, is_header=False):
         heading_label = tk.Label(master=header_frame, text=heading.upper())
         heading_label.grid(row=0, column=index, pady=5, padx=5, sticky="ew")
     return header_frame
+
+def build_results_section(master_frame):
+    results_section = tk.Frame(master=master_frame, bg="pink")
+    results_section.rowconfigure(0, weight=1, minsize=10)
+    results_section.columnconfigure(0, weight=1, minsize=10)
+    
+    footer_frame = tk.Frame(master=results_section, bg="purple")
+    previous_button = tk.Button(footer_frame, text="Previous", command=lambda: change_book_results_page(False))
+    next_button = tk.Button(footer_frame, text="Next", command=lambda: change_book_results_page(True))
+    page_label = tk.Label(footer_frame, text="Page")
+
+    books_page_state['page_label'] = page_label
+
+    previous_button.pack(fill=tk.Y, side=tk.LEFT)
+    next_button.pack(fill=tk.Y, side=tk.LEFT)
+    page_label.pack(fill=tk.Y, side=tk.RIGHT)
+
+    footer_frame.pack(fill=tk.X, side=tk.BOTTOM, expand=0)
+    books_page_state['results_section'] = results_section
+    return results_section
 
 def build_results_page():
     page_data = books_page_state['current_page'][1]
@@ -299,7 +259,7 @@ def build_results_page():
         new_row.grid(row=i, column=0, padx=5, pady=3, sticky="nesw")
     page_frame.pack(fill=tk.BOTH, side=tk.TOP, expand=1)
     books_page_state['current_page'][2] = page_frame
-    
+
 def build_results_row(master_frame, row_data):
     headings = books_page_state['result_headings']
 
@@ -341,6 +301,47 @@ def build_results_row(master_frame, row_data):
             row_label.grid(row=0, column=index, pady=5, padx=5, sticky="ew")
     return row_frame
 
+## Books Page Functionality ==============================================================
+def book_search_handler(search_phrase):
+    current_page = books_page_state['current_page']
+
+    if len(current_page) == 3:
+        books_page_state['current_page'][2].destroy()
+
+    search_results = bs.search_handler(search_phrase, books_page_state['selected_categories'])
+    # for key in search_results:
+    #     print(f"Page {key}:", search_results[key], sep="\n")
+
+    if search_results:
+        current_page = [0, search_results[0], None]
+        page_label = f"Page 1 of {len(search_results)}"
+    else:
+        current_page = [0, [], None]
+        page_label = "Page 1 of 1"
+
+    books_page_state['search_results'] = search_results
+    books_page_state['current_page'] = current_page
+    books_page_state['page_label']['text'] = page_label
+
+    build_results_page()
+
+def book_checkout_handler():
+    loan_duration = books_page_state['duration_var'].get()
+    member_id = books_page_state['member_var'].get()
+    selected_books = books_page_state['checkout_books']
+
+    if len(member_id) < 4 or int(member_id) < 1000:
+        alert("Please enter a valid member ID!!!")
+    else:
+        try:
+            member_id, loan_duration = int(member_id), int(loan_duration)
+            bc.checkout_handler(member_id, selected_books, loan_duration)
+            clear_selected_books()
+            book_search_handler(books_page_state['search_var'].get())
+            alert("Success - Book(s) have been checked out", False)
+        except:
+            alert("Error - Books could not be checked out")
+    
 def change_book_results_page(increment):
     page_num, page_data, page_frame = books_page_state['current_page']
     num_results = len(books_page_state['search_results'])
@@ -396,6 +397,14 @@ def clear_selected_books():
 
 def validate_member_entry(val):
     return re.match('^[0-9]*$', val) is not None and len(val) < 5
+
+# ===================================================================================== LOAN MANAGER PAGE =====================================================================================
+## Loan Manager State Variables =======================================================
+loan_manager_state = {}
+
+## Loan Manager UI Components =========================================================
+
+## Loan Manager Functionality =========================================================
 
 # ==================================================================================== MOVING BETWEEN PAGES ====================================================================================
 ### Assignments/function calls =======================================================
