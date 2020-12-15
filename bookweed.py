19138634efa319e38ace7642a3265b6441f8cf88
bheki-maenetja <bhekimaenetja@gmail.com>
2020-12-15 11:07:55 +0200

    building graphs on bookweed

--- bookweed.py
+++ bookweed.py
@@ -1,5 +1,5 @@
 # Standard Library Imports
-
+import matplotlib.pyplot as plt
 
 # Local Imports
 import database as db
@@ -9,4 +9,21 @@ def display_popular_titles(result_size=10):
     titles_usage_data = db.get_title_usage()[:result_size]
     titles, usage_data = [datum[0] for datum in titles_usage_data], [datum[1] for datum in titles_usage_data]
 
-display_popular_titles()
+    new_figure = plt.figure()
+    bar_graph = new_figure.add_subplot(1,1,1)
+    plt.title("This is the title right?")
+
+    bar_graph.bar(titles, usage_data)
+
+    plt.xticks(
+        rotation=45, 
+        horizontalalignment='right',
+        fontweight='light',
+        fontsize='x-small'  
+    )
+
+    plt.tight_layout()
+    return new_figure
+
+my_figure = display_popular_titles()
+plt.show()
