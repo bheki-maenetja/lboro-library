ec93627ba1f74bbbf78f5d83fd399fab8b48bdfe
bheki-maenetja <bhekimaenetja@gmail.com>
2020-12-17 03:42:12 +0200

    completed code clean up, comments and doc strings on bookcheckout and bookreturn

--- operations/bookcheckout.py
+++ operations/bookcheckout.py
@@ -1,7 +1,17 @@
 # Local Imports
 from system_data import database as db
 
-# =========================================================================== Functionality for Checking Out Books ===========================================================================
+# =================== Functionality for Checking Out Books ===================
 def checkout_handler(member_id, book_ids, loan_duration):
+    """
+    PARAMETERS
+        * member_id -> an integer representing the unique identifier of a library member
+        * book_ids -> a list of integers that represent the unique ids of selected books
+        * loan_duration -> an integer representing the number of days the selected books will be loaned out for
+    RETURN VALUES
+        * None
+    WHAT DOES THIS FUNCTION DO?
+        * This function loans out the selected books for the given loan_duration
+    """
     for book_id in book_ids:
         db.checkout_book(book_id, member_id, loan_duration)
--- operations/bookreturn.py
+++ operations/bookreturn.py
@@ -1,7 +1,15 @@
 # Local Imports
 from system_data import database as db
 
-# ============================================================================= Functionality for Returning Books =============================================================================
+# ===================== Functionality for Returning Books ====================
 def return_handler(selected_records):
+    """
+    PARAMETERS
+        * selected_records -> a list of dictionaries that represent information about books on loan
+    RETURN VALUES
+        * None
+    WHAT DOES THIS FUNCTION DO?
+        * This function returns, to the shelf, the books in the selected records
+    """
     for log_id, book_id in selected_records:
         db.return_book(log_id, book_id)
